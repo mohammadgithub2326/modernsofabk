85922097a1ee0ef4445c2a423bbc3200852a1cf2
mohammad noor <g.s.dadanoor@gmail.com>
2024-08-22 10:29:08 +0530

    semi coomit3

--- routes/active.js
+++ routes/active.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { active} = require('../controllers/active');
 
 // Route to create a new order
-router.post('/ready', active);
+router.get('/ready', active);
 
 
 module.exports = router;
